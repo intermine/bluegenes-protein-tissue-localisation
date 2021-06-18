4aa91b11dc8e40163da4816018916e16fe01ea93
uosl <regen@anche.no>
2021-06-18 19:10:47 +0100

    First working vega-lite heatmap

--- src/components/Heatmap.js
+++ src/components/Heatmap.js
@@ -2,10 +2,43 @@ import React from 'react';
 import { VegaLite } from 'react-vega';
 
 const Heatmap = ({ graphData, getLevel }) => {
+	const spec = {
+		$schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+		// width: 'container',
+		// height: { step: 10 },
+		// autosize: {
+		// 	type: 'fit-x',
+		// 	contains: 'padding'
+		// },
+		mark: 'rect',
+		encoding: {
+			row: { field: 'tissue' },
+			y: { field: 'gene', type: 'ordinal', title: null },
+			x: {
+				field: 'cell',
+				type: 'ordinal',
+				title: null,
+				axis: { orient: 'top', labelAngle: -45, labelAlign: 'left' }
+			},
+			color: {
+				field: 'expression',
+				type: 'quantitative',
+				scale: { scheme: 'blues' },
+				legend: null
+			},
+			tooltip: [
+				{ field: 'tissue', type: 'ordinal' },
+				{ field: 'gene', type: 'ordinal' },
+				{ field: 'cell', type: 'ordinal' },
+				{ field: 'expression', type: 'quantitative' }
+			]
+		},
+		data: { name: 'values' }
+	};
 
 	return (
 		<div className="graph-container">
-			<pre>{JSON.stringify(graphData, null, 2)}</pre>
+			<VegaLite spec={spec} data={{ values: graphData }} />
 		</div>
 	);
 };
